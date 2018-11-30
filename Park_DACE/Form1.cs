46c69d74990b9420579fdc49c44f15e388b008ea
jorgecorreia27 <2151571@my.ipleiria.pt>
2018-11-30 12:15:03 +0000

    O Miguel e muita bom

--- Park_DACE/Form1.cs
+++ Park_DACE/Form1.cs
@@ -18,21 +18,38 @@ namespace Park_DACE
             InitializeComponent();
         }
 
-        public static string ReadFromXLSFile(string filename, string ID)
+        public static string getGeolocationForGivenID(string ID)
         {
             
             var excelApp = new Excel.Application();
             excelApp.Visible = false;
 
+            string filename = @"C:\Campus_2_A_Park1.xlsx";
 
             //Open Excel file
-            var excellWorkbook = excelApp.Workbooks.Open(filename);
-            var excellWorksheet = (Excel.Worksheet)excellWorkbook.ActiveSheet;
+            Excel.Workbook excellWorkbook = excelApp.Workbooks.Open(filename);
+            Excel.Worksheet excellWorksheet = (Excel.Worksheet)excellWorkbook.ActiveSheet;
+
+            /*
+            int primeiraColuna = 1;
+            int segundaColuna = 2;
+            int indicePrimeiraLinha = 6;
 
             //Here we need to read the cells of the ID's and check if anyone matches 
             //If match, read geolocation
             //If not, show error maybe?
-            
+
+            if(excellWorksheet.Cells[indicePrimeiraLinha, primeiraColuna].Value2 != null)
+            {
+               
+                return excellWorksheet.Cells[indicePrimeiraLinha, primeiraColuna].Value2;
+            }
+            */
+
+            List<String> idsFromExcel = new List<string>();
+
+
+
             return "";
         }
     }
