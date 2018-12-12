09435d37a4a1a055fc487bc535ef781fb9db2828
MLiban10 <2151574@my.ipleiria.pt>
2018-12-12 12:55:54 +0000

    Mudei nomes e fiz um pouco do controller do smart park

---
+++ ParkSS/Models/ParkingSpot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smart_Park.Models
+{
+    public class ParkingSpot
+    {
+        public String Id { get; set; }
+        public String Type { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+        public int BateryStatus { get; set; }
+        public Boolean Value { get; set; }
+        public String Timestamp { get; set; }
+
+        public string ToString()
+        {
+            try
+            {
+                return string.Format("{0},{1},{2},{3},{4},{5},{6};", Id, Type, Name, Location, BateryStatus, Value, Timestamp);
+            }
+            catch (Exception)
+            {
+                string msg = string.Format("'{0}' is an invalid format string", Name);
+                throw new ArgumentException(msg);
+            }
+        }
+    }
+
+}
