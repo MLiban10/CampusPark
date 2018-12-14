fa7eaae271589f2daac24a5b950df50bb1fa7d6e
jorgecorreia27 <2151571@my.ipleiria.pt>
2018-12-14 12:06:43 +0000

    Criação do botao getAllSpots do admin Dashboard

--- ParkDashboard/Form1.Designer.cs
+++ ParkDashboard/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAllSpots = new System.Windows.Forms.Button();
+            this.richTextBoxSpots = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btnAllSpots
+            // 
+            this.btnAllSpots.Location = new System.Drawing.Point(13, 13);
+            this.btnAllSpots.Name = "btnAllSpots";
+            this.btnAllSpots.Size = new System.Drawing.Size(75, 23);
+            this.btnAllSpots.TabIndex = 0;
+            this.btnAllSpots.Text = "GetAllSpots";
+            this.btnAllSpots.UseVisualStyleBackColor = true;
+            this.btnAllSpots.Click += new System.EventHandler(this.btnAllSpots_Click);
+            // 
+            // richTextBoxSpots
+            // 
+            this.richTextBoxSpots.Location = new System.Drawing.Point(13, 42);
+            this.richTextBoxSpots.Name = "richTextBoxSpots";
+            this.richTextBoxSpots.Size = new System.Drawing.Size(189, 312);
+            this.richTextBoxSpots.TabIndex = 1;
+            this.richTextBoxSpots.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.richTextBoxSpots);
+            this.Controls.Add(this.btnAllSpots);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Park Dashboard";
             this.ResumeLayout(false);
@@ -42,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAllSpots;
+        private System.Windows.Forms.RichTextBox richTextBoxSpots;
     }
 }
 
--- ParkDashboard/Form1.cs
+++ ParkDashboard/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using ParkDashboard.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,25 @@ namespace ParkDashboard
 {
     public partial class Form1 : Form
     {
+        string baseURI = @"http://localhost:52566/";
+        HttpClient client = null;
+        List<ParkingSpot> products = new List<ParkingSpot>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAllSpots_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync($"api/spots").Result;
         }
     }
 }
