a1fb1c9499aee185caed1fa5b84f3bf0e1176f06
jorgecorreia27 <2151571@my.ipleiria.pt>
2018-12-14 13:33:28 +0000

    ParkDashboard implementado, estão 6 das 10 perguntas do rest nele implementadas

--- ParkDashboard/Form1.Designer.cs
+++ ParkDashboard/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.btnAllSpots = new System.Windows.Forms.Button();
             this.richTextBoxSpots = new System.Windows.Forms.RichTextBox();
+            this.btnReplaceSensors = new System.Windows.Forms.Button();
+            this.comboBoxSpots = new System.Windows.Forms.ComboBox();
+            this.btnSpotInfo = new System.Windows.Forms.Button();
+            this.comboBoxParks = new System.Windows.Forms.ComboBox();
+            this.btnParkPercentage = new System.Windows.Forms.Button();
+            this.btnParkSpots = new System.Windows.Forms.Button();
+            this.btnParkSensors = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAllSpots
             // 
-            this.btnAllSpots.Location = new System.Drawing.Point(13, 13);
+            this.btnAllSpots.Location = new System.Drawing.Point(208, 12);
             this.btnAllSpots.Name = "btnAllSpots";
             this.btnAllSpots.Size = new System.Drawing.Size(75, 23);
             this.btnAllSpots.TabIndex = 0;
@@ -44,20 +51,98 @@
             // 
             // richTextBoxSpots
             // 
-            this.richTextBoxSpots.Location = new System.Drawing.Point(13, 42);
+            this.richTextBoxSpots.Location = new System.Drawing.Point(13, 12);
             this.richTextBoxSpots.Name = "richTextBoxSpots";
-            this.richTextBoxSpots.Size = new System.Drawing.Size(189, 312);
+            this.richTextBoxSpots.Size = new System.Drawing.Size(189, 342);
             this.richTextBoxSpots.TabIndex = 1;
             this.richTextBoxSpots.Text = "";
+            // 
+            // btnReplaceSensors
+            // 
+            this.btnReplaceSensors.Enabled = false;
+            this.btnReplaceSensors.Location = new System.Drawing.Point(208, 42);
+            this.btnReplaceSensors.Name = "btnReplaceSensors";
+            this.btnReplaceSensors.Size = new System.Drawing.Size(75, 49);
+            this.btnReplaceSensors.TabIndex = 2;
+            this.btnReplaceSensors.Text = "Check Critical Sensors";
+            this.btnReplaceSensors.UseVisualStyleBackColor = true;
+            this.btnReplaceSensors.Click += new System.EventHandler(this.btnReplaceSensors_Click);
+            // 
+            // comboBoxSpots
+            // 
+            this.comboBoxSpots.FormattingEnabled = true;
+            this.comboBoxSpots.Location = new System.Drawing.Point(304, 12);
+            this.comboBoxSpots.Name = "comboBoxSpots";
+            this.comboBoxSpots.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSpots.TabIndex = 3;
+            // 
+            // btnSpotInfo
+            // 
+            this.btnSpotInfo.Enabled = false;
+            this.btnSpotInfo.Location = new System.Drawing.Point(304, 42);
+            this.btnSpotInfo.Name = "btnSpotInfo";
+            this.btnSpotInfo.Size = new System.Drawing.Size(121, 23);
+            this.btnSpotInfo.TabIndex = 4;
+            this.btnSpotInfo.Text = "Spot Info";
+            this.btnSpotInfo.UseVisualStyleBackColor = true;
+            this.btnSpotInfo.Click += new System.EventHandler(this.btnSpotInfo_Click);
+            // 
+            // comboBoxParks
+            // 
+            this.comboBoxParks.FormattingEnabled = true;
+            this.comboBoxParks.Location = new System.Drawing.Point(467, 14);
+            this.comboBoxParks.Name = "comboBoxParks";
+            this.comboBoxParks.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxParks.TabIndex = 5;
+            // 
+            // btnParkPercentage
+            // 
+            this.btnParkPercentage.Enabled = false;
+            this.btnParkPercentage.Location = new System.Drawing.Point(467, 42);
+            this.btnParkPercentage.Name = "btnParkPercentage";
+            this.btnParkPercentage.Size = new System.Drawing.Size(121, 36);
+            this.btnParkPercentage.TabIndex = 6;
+            this.btnParkPercentage.Text = "Get Ocupation Percentage for Park";
+            this.btnParkPercentage.UseVisualStyleBackColor = true;
+            this.btnParkPercentage.Click += new System.EventHandler(this.btnParkPercentage_Click);
+            // 
+            // btnParkSpots
+            // 
+            this.btnParkSpots.Enabled = false;
+            this.btnParkSpots.Location = new System.Drawing.Point(467, 84);
+            this.btnParkSpots.Name = "btnParkSpots";
+            this.btnParkSpots.Size = new System.Drawing.Size(121, 23);
+            this.btnParkSpots.TabIndex = 7;
+            this.btnParkSpots.Text = "Get Spots For Park";
+            this.btnParkSpots.UseVisualStyleBackColor = true;
+            this.btnParkSpots.Click += new System.EventHandler(this.btnParkSpots_Click);
+            // 
+            // btnParkSensors
+            // 
+            this.btnParkSensors.Enabled = false;
+            this.btnParkSensors.Location = new System.Drawing.Point(467, 114);
+            this.btnParkSensors.Name = "btnParkSensors";
+            this.btnParkSensors.Size = new System.Drawing.Size(121, 41);
+            this.btnParkSensors.TabIndex = 8;
+            this.btnParkSensors.Text = "Check Critical Sensors For Park";
+            this.btnParkSensors.UseVisualStyleBackColor = true;
+            this.btnParkSensors.Click += new System.EventHandler(this.btnParkSensors_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnParkSensors);
+            this.Controls.Add(this.btnParkSpots);
+            this.Controls.Add(this.btnParkPercentage);
+            this.Controls.Add(this.comboBoxParks);
+            this.Controls.Add(this.btnSpotInfo);
+            this.Controls.Add(this.comboBoxSpots);
+            this.Controls.Add(this.btnReplaceSensors);
             this.Controls.Add(this.richTextBoxSpots);
             this.Controls.Add(this.btnAllSpots);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Park Dashboard";
             this.ResumeLayout(false);
@@ -68,6 +153,13 @@
 
         private System.Windows.Forms.Button btnAllSpots;
         private System.Windows.Forms.RichTextBox richTextBoxSpots;
+        private System.Windows.Forms.Button btnReplaceSensors;
+        private System.Windows.Forms.ComboBox comboBoxSpots;
+        private System.Windows.Forms.Button btnSpotInfo;
+        private System.Windows.Forms.ComboBox comboBoxParks;
+        private System.Windows.Forms.Button btnParkPercentage;
+        private System.Windows.Forms.Button btnParkSpots;
+        private System.Windows.Forms.Button btnParkSensors;
     }
 }
 
--- ParkDashboard/Form1.cs
+++ ParkDashboard/Form1.cs
@@ -16,11 +16,18 @@ namespace ParkDashboard
     {
         string baseURI = @"http://localhost:52566/";
         HttpClient client = null;
-        List<ParkingSpot> products = new List<ParkingSpot>();
+        List<ParkingSpot> parkingSpots = new List<ParkingSpot>();
+
+        
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public String ShowParkingSpotsAll(ParkingSpot p)
+        {
+            return $"{p.Id} \n";
         }
 
         private void btnAllSpots_Click(object sender, EventArgs e)
@@ -33,6 +40,195 @@ namespace ParkDashboard
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
             HttpResponseMessage response = client.GetAsync($"api/spots").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                //converter a resposta que esta em JSON para uma List<ParkingSpot>
+                string json = response.Content.ReadAsStringAsync().Result;
+                parkingSpots = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ParkingSpot>>(json);
+
+                foreach (ParkingSpot parkingSpot in parkingSpots)
+                {
+                    richTextBoxSpots.AppendText(ShowParkingSpotsAll(parkingSpot));
+                }
+                btnReplaceSensors.Enabled = true;
+                this.comboBoxSpots.DataSource = parkingSpots;
+                this.comboBoxSpots.DisplayMember = "Id";
+                this.comboBoxSpots.ValueMember = "Id";
+                btnSpotInfo.Enabled = true;
+
+                var dataSource = new List<Park>();
+                dataSource.Add(new Park() { name = "Campus_2_A_Park1" });
+                dataSource.Add(new Park() { name = "Campus_2_B_Park1" });
+
+                this.comboBoxParks.DataSource = dataSource;
+                this.comboBoxParks.DisplayMember = "Name";
+                this.comboBoxParks.ValueMember = "Name";
+
+                btnParkPercentage.Enabled = true;
+                btnParkSpots.Enabled = true;
+                btnParkSensors.Enabled = true;
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
+        }
+
+        public String ShowParkingSpotsBatteryReplacement(ParkingSpot p)
+        {
+            return $"{p.Id}\nBattery Status: Critical!!\n---------------------------------------------------------";
+        }
+
+        private void btnReplaceSensors_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync($"api/spots/critical").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                parkingSpots = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ParkingSpot>>(json);
+
+                foreach (ParkingSpot parkingSpot in parkingSpots)
+                {
+                    richTextBoxSpots.AppendText(ShowParkingSpotsBatteryReplacement(parkingSpot));
+                }
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
+        }
+
+        public String ShowFullParkingSpot(ParkingSpot p)
+        {
+            return $"ID :{p.Id}\nValue: {p.Value}\nName: {p.Name}\nLocation: {p.Location}\nType: {p.Type}\nBattery Status: {p.BateryStatus}\nTime: {p.Timestamp}\n---------------------------------------------------------\n";
+        }
+
+        private void btnSpotInfo_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            string id = comboBoxSpots.SelectedValue.ToString();
+
+            HttpResponseMessage response = client.GetAsync($"api/spots/{id}").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                ParkingSpot result = Newtonsoft.Json.JsonConvert.DeserializeObject<ParkingSpot>(json);
+                richTextBoxSpots.AppendText(ShowFullParkingSpot(result));
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
+        }
+
+        private void btnParkPercentage_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            string id = comboBoxParks.SelectedValue.ToString();
+
+            HttpResponseMessage response = client.GetAsync($"api/spots/parks/{id}/ocupation").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                
+                richTextBoxSpots.AppendText(json);
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
+
+        }
+
+        private void btnParkSpots_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            string id = comboBoxParks.SelectedValue.ToString();
+
+            HttpResponseMessage response = client.GetAsync($"api/spots/parks/{id}").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                parkingSpots = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ParkingSpot>>(json);
+
+                foreach (ParkingSpot parkingSpot in parkingSpots)
+                {
+                    richTextBoxSpots.AppendText(ShowFullParkingSpot(parkingSpot));
+                }
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
+        }
+
+        private void btnParkSensors_Click(object sender, EventArgs e)
+        {
+            richTextBoxSpots.Text = "";
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            string id = comboBoxParks.SelectedValue.ToString();
+
+            HttpResponseMessage response = client.GetAsync($"api/spots/critical/parks/{id}").Result;
+
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                parkingSpots = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ParkingSpot>>(json);
+
+                if (!parkingSpots.Any())
+                {
+                    richTextBoxSpots.AppendText("There are no sensors in need to be replaced");
+                }
+                else
+                {
+                    foreach (ParkingSpot parkingSpot in parkingSpots)
+                    {
+                        richTextBoxSpots.AppendText(ShowParkingSpotsBatteryReplacement(parkingSpot));
+                    }
+                }
+                
+            }
+            else
+            {
+                richTextBoxSpots.AppendText($"Error connecting to API {response.StatusCode} with message {response.ReasonPhrase}.");
+            }
         }
     }
 }
